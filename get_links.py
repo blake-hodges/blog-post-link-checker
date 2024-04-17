25a558fe9e2b49ceea0b96132fb2f1f4c834a588
Blake Hodges <tbhodges@gmail.com>
2024-04-17 11:05:19 -0400

    add requirements.txt for python libraries that need to be imported

--- get_links.py
+++ get_links.py
@@ -1,12 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
+#change the url to the webpage you want to get the links from
 url = "https://blog.pragmaticengineer.com/italian-bank-outage/"
 
 response = requests.get(url)
 
+#use the beautiful soup html parser
 soup = BeautifulSoup(response.text, 'html.parser')
+
+#save all the links from the webpage
 links = soup.find_all('a')
+
+#loop through the links and print to the console
 for link in links:
     href = link.get('href')
     print(href)
